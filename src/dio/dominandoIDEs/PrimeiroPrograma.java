84abf40f82122bb4be0edb75a4f4fc0616bde8de
Rodrigo Ferreira Pereira <rudrigopereira@gmail.com>
2022-10-12 23:47:07 -0300

    primeiro commit
    dominando IDEs java

---
+++ src/dio/dominandoIDEs/PrimeiroPrograma.java
@@ -0,0 +1,52 @@
+package dio.dominandoIDEs;
+
+import dio.dominandoIDEs.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        Livro livro = new Livro();
+        System.out.println(livro);
+
+//        int a=5, b=3;
+//        System.out.println("Hello, World! " + (a+b));
+    }
+}
+
+class Livro{
+    private String nome;
+    private int numPaginas;
+
+    public Livro() {}
+
+    public Livro(String nome, int numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(int numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+}
